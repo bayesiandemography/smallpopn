23fe6a74f3cbd5bbed8dd5032a8e3942b3f3c4b3
John Bryant <john.r.bryant@gmail.com>
2023-08-28 11:37:57 +1200

    added readme and documentation for package

--- R/smallpopn-package.R
+++ R/smallpopn-package.R
@@ -1,3 +1,18 @@
+#' Package 'smallpopn'
+#' 
+#' Functions needed for paper "How big does a population
+#' need to be before demographers can ignore individual-level
+#' randomness in demographic events?"
+#'
+#' @section Functions:
+#'
+#' - `Lx` - Life table values for West model life table.
+#' - `microsim` - Function to carry out demographic microsimulation.
+#' - `mx` - Mortality rates for West model life table.
+#' - `propn_age_fert` - Age distribution of fertility from Booth standard
+#'
+#' @keywords internal
+"_PACKAGE"
 
 ## usethis namespace: start
 #' @importFrom Rcpp evalCpp
---
+++ README.md
@@ -0,0 +1,15 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# smallpopn
+
+Functions and data used in the paper “How big does a population need to
+be before demographers can ignore individual-level randomness in
+demographic events?”
+
+## Installation
+
+``` r
+# install.packages("devtools")
+devtools::install_github("bayesiandemography/smallpopn")
+```
